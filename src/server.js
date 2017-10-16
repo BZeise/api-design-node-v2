4753e51f02d9ac381e770995ef4439d4f0fdcb7d
Ben Zeise <ben.zeise@gmail.com>
2017-10-16 10:38:34 -0500

    working on lesson-1-solution

--- src/server.js
+++ src/server.js
@@ -6,15 +6,22 @@ import { signin, protect } from './api/modules/auth'
 // Declare an app from express
 const app = express()
 
+const apiRouter = express.Router()
+
+apiRouter.get('/', (req, res) => res.json({api: true}))
+apiRouter.get('*', (req, res) => res.json({apiAll: true}))
+
 setupMiddware(app)
 connect()
 // setup basic routing for index route
 
 app.use('/signin', signin)
 
+app.use('/api', apiRouter)
+
 // catch all
 app.all('*', (req, res) => {
-  res.json({ok: true})
+  res.json({ok: true, foo: 'bar'})
 })
 
 export default app
